0ee482c92a2eda5e5f069e1f9d9348424be7646e
Marcus Döllerer <marcus@ak5s.com>
2019-11-17 21:52:39 +0100

    Changes for Symfony >= 3.2.0

--- AdamQuaileFieldsetBundle.php
+++ AdamQuaileFieldsetBundle.php
@@ -11,9 +11,6 @@ class AdamQuaileFieldsetBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
         $container->addCompilerPass(new FieldsetCompilerPass());
-
     }
-
 }
